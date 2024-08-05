bbac31b19535699df5487a2772df3afbccdb0472
insane3000 <58342180+insane3000@users.noreply.github.com>
2024-08-04 21:44:31 -0400

    finished

--- src/store/apiKeyStore.ts
+++ src/store/apiKeyStore.ts
@@ -8,7 +8,7 @@ interface ApiKeyStoreIT {
 }
 
 export const useApiKeyStore = create<ApiKeyStoreIT>((set) => ({
-  apiKey: "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",
+  apiKey: "",
   setApiKey: (value: "") => set((state: ApiKeyStoreIT) => ({ ...state, apiKey: value })),
   modelInput: "llama-3-sonar-small-32k-chat",
   setModelInput: (value: "") => set((state: ApiKeyStoreIT) => ({ ...state, modelInput: value })),
